8e279021ef96fe11f2d06f1f87a73bd040a6b4db
JstGrizz <khalidu234@gmail.com>
2025-06-17 01:54:24 +0700

    Added Icon for Sidebar

--- app/Views/sidebar.php
+++ app/Views/sidebar.php
@@ -10,7 +10,7 @@ $currentUri2 = $request->getUri()->getSegment(2);
         <div class="sidebar-header position-relative">
             <div class="d-flex justify-content-between align-items-center">
                 <div class="logo">
-                    <a href="#"><img src=".../public/assets/Image/icon.png" alt="Logo" srcset="" /></a>
+                    <img src="<?= base_url('assets/Image/icon.png') ?>" alt="Logo" width="100" style="height:auto;" />
                 </div>
                 <div class="theme-toggle d-flex gap-2 align-items-center mt-2">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
